565a9b8e72b3a7570843d86b9ef807c888191af4
Victor Saiz <vs@vsaiz.me>
2017-09-20 16:48:34 +0200

    fix scroll layout

--- src/app/pages/local/local.jsx
+++ src/app/pages/local/local.jsx
@@ -6,71 +6,69 @@ import pageStyles from 'styles/pages.scss'
 
 const Local = () => (
   <div className={cx(styles.container, pageStyles.container)}>
-    <div className={cx(pageStyles.scroller)}>
-      <h1 className={cx(styles.title, pageStyles.titles)}>
-        Okavango wilderness project
-      </h1>
-      <p className={cx(pageStyles.content)}>
-        In September 2013, we embarked on our most challenging crossing of the
-        Okavango Delta… The research data set we achieved over 15 days and 338km
-        using a bespoke Android App was the most comprehensive ever and was
-        shared in real-time via an open API and up-to-date satellite image. This
-        10-minute video documents an impossible expedition across dry, abandoned
-        floodplains and crocodile- and hippopotamus-infested channels flanked by
-        the abundance of life. Astonishingly, red lechwe, an aquatic antelope
-        only found in the Okavango Delta, were the second most frequently
-        sighted after squacco herons and African jacanas, the most abundant
-        wetland birds in the region. Thousands of buffalo, giraffe, zebra,
-        tsetsebe, kudu, elephant, baboon, lion, hyena, wild dog and much else
-        gathered on the islands nearby the last flowing channels in the central
-        wilderness of Africa’s last-remaining wetland wilderness.
-      </p>
-      <p className={cx(pageStyles.content)}>
-        In September 2013, we embarked on our most challenging crossing of the
-        Okavango Delta… The research data set we achieved over 15 days and 338km
-        using a bespoke Android App was the most comprehensive ever and was
-        shared in real-time via an open API and up-to-date satellite image. This
-        10-minute video documents an impossible expedition across dry, abandoned
-        floodplains and crocodile- and hippopotamus-infested channels flanked by
-        the abundance of life. Astonishingly, red lechwe, an aquatic antelope
-        only found in the Okavango Delta, were the second most frequently
-        sighted after squacco herons and African jacanas, the most abundant
-        wetland birds in the region. Thousands of buffalo, giraffe, zebra,
-        tsetsebe, kudu, elephant, baboon, lion, hyena, wild dog and much else
-        gathered on the islands nearby the last flowing channels in the central
-        wilderness of Africa’s last-remaining wetland wilderness.
-      </p>
-      <p className={cx(pageStyles.content)}>
-        In September 2013, we embarked on our most challenging crossing of the
-        Okavango Delta… The research data set we achieved over 15 days and 338km
-        using a bespoke Android App was the most comprehensive ever and was
-        shared in real-time via an open API and up-to-date satellite image. This
-        10-minute video documents an impossible expedition across dry, abandoned
-        floodplains and crocodile- and hippopotamus-infested channels flanked by
-        the abundance of life. Astonishingly, red lechwe, an aquatic antelope
-        only found in the Okavango Delta, were the second most frequently
-        sighted after squacco herons and African jacanas, the most abundant
-        wetland birds in the region. Thousands of buffalo, giraffe, zebra,
-        tsetsebe, kudu, elephant, baboon, lion, hyena, wild dog and much else
-        gathered on the islands nearby the last flowing channels in the central
-        wilderness of Africa’s last-remaining wetland wilderness.
-      </p>
-      <p className={cx(pageStyles.content)}>
-        In September 2013, we embarked on our most challenging crossing of the
-        Okavango Delta… The research data set we achieved over 15 days and 338km
-        using a bespoke Android App was the most comprehensive ever and was
-        shared in real-time via an open API and up-to-date satellite image. This
-        10-minute video documents an impossible expedition across dry, abandoned
-        floodplains and crocodile- and hippopotamus-infested channels flanked by
-        the abundance of life. Astonishingly, red lechwe, an aquatic antelope
-        only found in the Okavango Delta, were the second most frequently
-        sighted after squacco herons and African jacanas, the most abundant
-        wetland birds in the region. Thousands of buffalo, giraffe, zebra,
-        tsetsebe, kudu, elephant, baboon, lion, hyena, wild dog and much else
-        gathered on the islands nearby the last flowing channels in the central
-        wilderness of Africa’s last-remaining wetland wilderness.
-      </p>
-    </div>
+    <h1 className={cx(styles.title, pageStyles.titles)}>
+      Okavango wilderness project
+    </h1>
+    <p className={cx(pageStyles.content)}>
+      In September 2013, we embarked on our most challenging crossing of the
+      Okavango Delta… The research data set we achieved over 15 days and 338km
+      using a bespoke Android App was the most comprehensive ever and was shared
+      in real-time via an open API and up-to-date satellite image. This
+      10-minute video documents an impossible expedition across dry, abandoned
+      floodplains and crocodile- and hippopotamus-infested channels flanked by
+      the abundance of life. Astonishingly, red lechwe, an aquatic antelope only
+      found in the Okavango Delta, were the second most frequently sighted after
+      squacco herons and African jacanas, the most abundant wetland birds in the
+      region. Thousands of buffalo, giraffe, zebra, tsetsebe, kudu, elephant,
+      baboon, lion, hyena, wild dog and much else gathered on the islands nearby
+      the last flowing channels in the central wilderness of Africa’s
+      last-remaining wetland wilderness.
+    </p>
+    <p className={cx(pageStyles.content)}>
+      In September 2013, we embarked on our most challenging crossing of the
+      Okavango Delta… The research data set we achieved over 15 days and 338km
+      using a bespoke Android App was the most comprehensive ever and was shared
+      in real-time via an open API and up-to-date satellite image. This
+      10-minute video documents an impossible expedition across dry, abandoned
+      floodplains and crocodile- and hippopotamus-infested channels flanked by
+      the abundance of life. Astonishingly, red lechwe, an aquatic antelope only
+      found in the Okavango Delta, were the second most frequently sighted after
+      squacco herons and African jacanas, the most abundant wetland birds in the
+      region. Thousands of buffalo, giraffe, zebra, tsetsebe, kudu, elephant,
+      baboon, lion, hyena, wild dog and much else gathered on the islands nearby
+      the last flowing channels in the central wilderness of Africa’s
+      last-remaining wetland wilderness.
+    </p>
+    <p className={cx(pageStyles.content)}>
+      In September 2013, we embarked on our most challenging crossing of the
+      Okavango Delta… The research data set we achieved over 15 days and 338km
+      using a bespoke Android App was the most comprehensive ever and was shared
+      in real-time via an open API and up-to-date satellite image. This
+      10-minute video documents an impossible expedition across dry, abandoned
+      floodplains and crocodile- and hippopotamus-infested channels flanked by
+      the abundance of life. Astonishingly, red lechwe, an aquatic antelope only
+      found in the Okavango Delta, were the second most frequently sighted after
+      squacco herons and African jacanas, the most abundant wetland birds in the
+      region. Thousands of buffalo, giraffe, zebra, tsetsebe, kudu, elephant,
+      baboon, lion, hyena, wild dog and much else gathered on the islands nearby
+      the last flowing channels in the central wilderness of Africa’s
+      last-remaining wetland wilderness.
+    </p>
+    <p className={cx(pageStyles.content)}>
+      In September 2013, we embarked on our most challenging crossing of the
+      Okavango Delta… The research data set we achieved over 15 days and 338km
+      using a bespoke Android App was the most comprehensive ever and was shared
+      in real-time via an open API and up-to-date satellite image. This
+      10-minute video documents an impossible expedition across dry, abandoned
+      floodplains and crocodile- and hippopotamus-infested channels flanked by
+      the abundance of life. Astonishingly, red lechwe, an aquatic antelope only
+      found in the Okavango Delta, were the second most frequently sighted after
+      squacco herons and African jacanas, the most abundant wetland birds in the
+      region. Thousands of buffalo, giraffe, zebra, tsetsebe, kudu, elephant,
+      baboon, lion, hyena, wild dog and much else gathered on the islands nearby
+      the last flowing channels in the central wilderness of Africa’s
+      last-remaining wetland wilderness.
+    </p>
   </div>
 )
 
