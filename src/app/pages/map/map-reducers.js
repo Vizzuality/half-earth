0e01f110e68b3fe0e9442811ae2480d1911f3532
Victor Saiz <vs@vsaiz.me>
2017-09-28 12:53:55 +0200

    Fix reducer error

--- src/app/pages/map/map-reducers.js
+++ src/app/pages/map/map-reducers.js
@@ -6,7 +6,7 @@ import * as actions from './map-actions'
 import { actions as cartoActions } from 'providers/carto'
 
 const updateLayer = (state, { payload, ...rest }) => {
-  const { name } = payload
+  const { name } = rest
   const { layers } = state
   const layer = find(layers, { name })
   const filtered = difference(layers, [layer])
