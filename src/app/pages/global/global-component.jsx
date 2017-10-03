afd45b4075bd61af3390f3c9181cea7eeaea3555
Victor Saiz <vs@vsaiz.me>
2017-10-03 12:12:21 +0200

    polish dropdown css

--- src/app/pages/global/global-component.jsx
+++ src/app/pages/global/global-component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import SpiderChart from 'components/spider-chart'
+import Placeholder from 'components/placeholder'
 import pageStyles from 'styles/pages.scss'
 
 class Global extends Component {
@@ -23,9 +24,8 @@ class Global extends Component {
         </p>
         <p>
           Thanks to them we now have data for all of the vertebrate species
-          known to science, {d('vertebrates')} including, {t('amphibians')},{' '}
-          {t('reptiles', '', true)}, {t('birds')} and {t('mammals')}; as well as
-          the major groups of {t('invertebrates', '', true)}, including{' '}
+          known to science, like {d('vertebrates')}; as well as the major groups
+          of {t('invertebrates', '', true)}, including{' '}
           {t('Butterflies', '', true)}, {t('Dragonflies', '', true)},{' '}
           {t('Moths', '', true)} and {t('Bumblebees', '', true)}. We also have
           comprehensive records for plant species in the {t('Conifers')},{' '}
@@ -34,13 +34,19 @@ class Global extends Component {
           they are an integral part of the network of life and are vulnerable to
           the same threats as the species we have records for.
         </p>
+        <SpiderChart />
         <p>
           {t('Protected Areas')} have a key role in the conservation of this
           network of life. Within their boundaries, protected areas harbour
           14.7% of terrestrial and inland water areas, 4.12% of the global
           ocean, and 10.2% of coastal and marine areas.
         </p>
-        <SpiderChart />
+        <Placeholder
+          name="multisparkline"
+          width="100%"
+          height="300px"
+          border="1px solid white"
+        />
         <p>
           The rate of extinction today is at least 1,000 times higher than at
           any time in Earth’s history but conservation efforts can reduce that
@@ -48,6 +54,12 @@ class Global extends Component {
           that will accompany advances in data collection and analyses, we’ll
           have the details we need to scale up our conservation efforts to a
           global level that encompasses half of the Earth.
+        </p>
+        <p>
+          With the information we already have, plus the influx of data that
+          will accompany advances in data collection and analyses, we’ll have
+          the details we need to scale up our conservation efforts to a global
+          level that encompasses half of the Earth.
         </p>
       </div>
     )
