72c494e7636e4146a87cbd1674c88aee61788555
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-10-13 15:42:30 +0200

    feat: adds video and event handler to intro page

--- src/app/pages/intro/intro.js
+++ src/app/pages/intro/intro.js
@@ -1,1 +1,29 @@
-export { default } from './intro-component'
+import { createElement, Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+import IntroComponent from './intro-component'
+
+class IntroContainer extends Component {
+  componentDidMount () {
+    this.player.addEventListener('ended', this.onVideoEnds.bind(this))
+  }
+
+  onVideoEnds = () => {
+    this.props.history.push('/local')
+  }
+
+  getRef = el => {
+    this.player = el
+  }
+
+  render () {
+    const { getRef } = this
+    return createElement(IntroComponent, {
+      ...this.props,
+      getRef,
+      videoSrc: 'http://media.w3.org/2010/05/bunny/trailer.mp4'
+    })
+  }
+}
+
+export default withRouter(IntroContainer)
