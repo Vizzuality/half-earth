a8bddc572b46878876943e63a39acbf40f2c1b96
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-10-16 15:01:52 +0200

    feat: adds background prop to text-content

--- src/app/components/pop-up/text-content/text-content-component.jsx
+++ src/app/components/pop-up/text-content/text-content-component.jsx
@@ -5,9 +5,12 @@ import capitalize from 'lodash/capitalize'
 import styles from './text-content-styles.scss'
 
 const TextContent = ({ className, content }) => {
-  const { details, description, image } = content
+  const { details, description, image, background } = content
   return (
-    <div className={cx([className, styles.textContent])}>
+    <div
+      className={cx([className, styles.textContent])}
+      style={{ backgroundImage: `url(${background})` }}
+    >
       <div
         className={styles.image}
         style={{ backgroundImage: `url(${image})` }}
