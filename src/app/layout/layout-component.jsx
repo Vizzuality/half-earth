675887691f7309c24555a21a31f74e325bdfccfd
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-10-17 16:49:04 +0200

    feat: includes simple legend styles

--- src/app/layout/layout-component.jsx
+++ src/app/layout/layout-component.jsx
@@ -37,16 +37,53 @@ const Layout = ({ children, location, sidebar, ...props }) => {
         {isHome && <Home />}
         {isIntro && <Intro />}
         <Map className={cx(styles.col, styles.map)} zoomLevel={route} />
-        {!isHome &&
-        !isIntro && (
+        {route === 'local' && (
           <Legend>
-            <h3>Title</h3>
-            <p>Paragraph</p>
-            <ul>
-              <li>List item</li>
-              <li>List item</li>
-              <li>List item</li>
-            </ul>
+            <div className={styles.legendContent}>
+              <span
+                className={cx([styles.simpleLegend, styles.simpleLegendRed])}
+              >
+                Roads
+              </span>
+              <span
+                className={cx([styles.simpleLegend, styles.simpleLegendBrown])}
+              >
+                Urban Development
+              </span>
+              <span
+                className={cx([styles.simpleLegend, styles.simpleLegendPink])}
+              >
+                Protected Areas
+              </span>
+              <span
+                className={cx([styles.simpleLegend, styles.simpleLegendOrange])}
+              >
+                Private Reserves
+              </span>
+              <span
+                className={cx([styles.simpleLegend, styles.simpleLegendGreen])}
+              >
+                Key Biodiversity Areas
+              </span>
+              <span
+                className={cx([styles.simpleLegend, styles.simpleLegendYellow])}
+              >
+                Where to Protect
+              </span>
+              <div className={styles.legendGradient}>
+                Biodiversity
+                <div className={styles.gradient}>
+                  All Taxa
+                  <span />
+                  <span />
+                  <span />
+                  <span />
+                  <span />
+                  <span />
+                  <span />
+                </div>
+              </div>
+            </div>
           </Legend>
         )}
       </div>
