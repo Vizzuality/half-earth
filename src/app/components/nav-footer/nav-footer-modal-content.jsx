b127262b8f7d8c77f28ed0e81b3ea9064900f57b
Hector <hi@hectorarce.es>
2017-10-19 16:54:59 +0200

    include links

---
+++ src/app/components/nav-footer/nav-footer-modal-content.jsx
@@ -0,0 +1,278 @@
+import React from 'react'
+import styles from './nav-footer-styles.scss'
+
+const ModalContent = props => (
+  <div className={styles.footerModal}>
+    <div className={styles.containt}>
+      <h1>Data Partners</h1>
+      <div className={styles.containImages}>
+        <a
+          href="http://www.amstat.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/1.png" alt="ASA" />
+        </a>
+        <a
+          href="http://www.birdlife.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/2.png"
+            alt="BirdLife International"
+          />
+        </a>
+        <a
+          href="https://www.rspb.org.uk/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/3.png" alt="rspb" />
+        </a>
+        <a
+          href="https://www.conservation.cam.ac.uk/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/4.png"
+            alt="Cambridge conservation initiative"
+          />
+        </a>
+        <a
+          href="https://www.conservation.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/5.png"
+            alt="conservation international"
+          />
+        </a>
+        <a
+          href="http://www.dlr.de/dlr/en/desktopdefault.aspx/tabid-10002/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/6.png" alt="dlr" />
+        </a>
+        <a
+          href="https://www.gbif.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/7.png" alt="gbif" />
+        </a>
+        <a
+          href="https://www.thegef.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/8.png" alt="gef" />
+        </a>
+        <a
+          href="https://www.globalwildlife.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/9.png"
+            alt="global wildlife conservation"
+          />
+        </a>
+        <a
+          href="https://www.islandconservation.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/10.png"
+            alt="island conservation"
+          />
+        </a>
+        <a
+          href="https://www.iucn.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/11.png" alt="iucn" />
+        </a>
+        <a
+          href="http://luchoffmanninstitute.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/12.png"
+            alt="luc hoffmann institute"
+          />
+        </a>
+        <a
+          href="http://www.gorongosa.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/13.png" alt="gorongosa" />
+        </a>
+        <a
+          href="http://tesf.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/14.png" alt="tesf" />
+        </a>
+        <a
+          href="http://www.nhm.ac.uk/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/15.png"
+            alt="natural history museum"
+          />
+        </a>
+        <a
+          href="http://www.natureserve.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/16.png" alt="nature serve" />
+        </a>
+        <a
+          href="https://www.nature.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/17.png"
+            alt="the nature conservancy"
+          />
+        </a>
+        <a
+          href="http://www.tompkinsconservation.org/home.htm"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/18.png"
+            alt="tompkings conservation"
+          />
+        </a>
+        <a
+          href="https://www.unep-wcmc.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/19.png" alt="unep-wcmc" />
+        </a>
+        <a
+          href="https://www.google.es/search?q=wcs&oq=wcs&aqs=chrome..69i57j0l5.789j0j4&sourceid=chrome&ie=UTF-8"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/21.png" alt="wcs" />
+        </a>
+        <a
+          href="https://www.worldwildlife.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/22.png" alt="wwf" />
+        </a>
+      </div>
+
+      <h1>Research Partners</h1>
+      <div className={styles.containImages}>
+        <a
+          href="https://www.amnh.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/23.png"
+            alt="American Museum of Natural History"
+          />
+        </a>
+        <a
+          href="https://www.calacademy.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/24.png"
+            alt="California academy of sciences"
+          />
+        </a>
+        <a
+          href="http://www.missouribotanicalgarden.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/26.png"
+            alt="missouri botanical garden"
+          />
+        </a>
+        <a
+          href="http://www.mcz.harvard.edu/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img
+            src="/img/partners/partner_image/27.png"
+            alt="museum of comparative zoology"
+          />
+        </a>
+        <a
+          href="http://www.mncn.csic.es/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/28.png" alt="mncn" />
+        </a>
+        <a
+          href="https://www.nybg.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/25.png" alt="nybg" />
+        </a>
+        <a
+          href="http://www.esf.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/29.png" alt="esf" />
+        </a>
+        <a
+          href="http://dlia.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/30.png" alt="DLiA" />
+        </a>
+      </div>
+
+      <h1>Engagement</h1>
+      <div className={styles.containImages}>
+        <a
+          href="http://www.msichicago.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/31.png" alt="msic" />
+        </a>
+        <a
+          href="http://www.opsociety.org/"
+          target="_blank"
+          className={styles.imageContainer}
+        >
+          <img src="/img/partners/partner_image/32.png" alt="OPS" />
+        </a>
+      </div>
+    </div>
+  </div>
+)
+
+export default ModalContent
