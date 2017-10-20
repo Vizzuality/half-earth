3a55c1ad2f359f4d90569876b8b0e6d17222fced
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-10-20 12:15:43 +0200

    feat: add minimum cap on slider

--- src/app/components/earthometer/earthometer-reducers.js
+++ src/app/components/earthometer/earthometer-reducers.js
@@ -3,5 +3,5 @@ import { assign } from 'app/utils'
 
 export default {
   [actions.setEarthSaved]: (state, { payload }) =>
-    assign(state, { value: Math.min(payload, 50) })
+    assign(state, { value: Math.max(Math.min(payload, 50), 15) })
 }
