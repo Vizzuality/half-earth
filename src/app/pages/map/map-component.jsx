1525dcb8371b4d277e7b165fa3cb0e7ab6a66e1f
Victor Saiz <vs@vsaiz.me>
2017-10-20 19:47:33 +0200

    WIP birds animation

--- src/app/pages/map/map-component.jsx
+++ src/app/pages/map/map-component.jsx
@@ -22,6 +22,45 @@ const Map = ({
     zoomLevel={zoomLevel}
     rotate={zoomLevel === 'global'}
   >
+    {zoomLevel === 'local' &&
+      local.billboards.map(billboard => (
+        <Billboard
+          key={billboard.id}
+          id={billboard.id}
+          url={billboard.url}
+          urlHover={billboard.urlHover}
+          width={82}
+          height={108}
+          onClick={id => openPopUp(id)}
+          position={billboard.coordinates}
+        />
+      ))}
+    {zoomLevel === 'loscal' && (
+      <Birds
+        log={console.log(section)}
+        pixelSize={15}
+        maxspeed={0.5}
+        target={[21.814053, -33.583702]}
+        north={-17.3312}
+        south={-20.273157}
+        east={24.031766}
+        west={21.395792}
+        numBirds="100"
+        url="hum"
+      />
+    )}
+    {(zoomLevel === 'local' || zoomLevel === 'regional') && (
+      <Birds
+        pixelSize={15}
+        target={[23.269086, -19.925504]}
+        north={7.8621481}
+        south={-34.5355066}
+        east={34.898694}
+        west={17.330078}
+        numBirds="200"
+        url="hum"
+      />
+    )}
     {zoomLevel === 'regional' &&
       regional.layers.map(
         layer =>
@@ -34,20 +73,6 @@ const Map = ({
         layer =>
           layer.url ? <ImageProvider key={layer.name} {...layer} /> : null
       )}
-    {zoomLevel === 'local' &&
-      local.billboards.map(billboard => (
-        <Billboard
-          key={billboard.id}
-          id={billboard.id}
-          url={billboard.url}
-          urlHover={billboard.urlHover}
-          width={82}
-          height={123}
-          onClick={id => openPopUp(id)}
-          position={billboard.coordinates}
-        />
-      ))}
-    {zoomLevel === 'regional' && <Birds numBirds="200" url="hum" />}
   </CesiumMap>
 )
 
