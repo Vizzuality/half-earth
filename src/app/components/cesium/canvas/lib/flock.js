b40047037cbf076976a6f2043ebf6fedc4920961
Victor Saiz <vs@vsaiz.me>
2017-10-20 12:19:16 +0200

    add mistakenly ignored files

---
+++ src/app/components/cesium/canvas/lib/flock.js
@@ -0,0 +1,18 @@
+// The Flock (a list of Boid objects)
+
+class Flock {
+  constructor () {
+    this.boids = [] // Initialize the ArrayList
+  }
+
+  run (renderBoid) {
+    const { boids } = this
+    boids.map(boid => renderBoid(boid.update(boids)))
+  }
+
+  addBoid (b) {
+    this.boids.push(b)
+  }
+}
+
+export default Flock
