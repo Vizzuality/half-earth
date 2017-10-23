5a416e7fefa1b1578fc209c2a798bf686e8a91f1
Victor Saiz <vs@vsaiz.me>
2017-10-23 17:38:54 +0200

    fix stagging config

--- stagging-server.js
+++ stagging-server.js
@@ -1,7 +1,9 @@
 const connect = require('connect')
 const path = require('path')
 const serveStatic = require('serve-static')
-
+const port = process.env.PORT || 8080
 connect()
   .use(serveStatic(path.join(__dirname, 'public')))
-  .listen(process.env.PORT || 8080)
+  .listen(port)
+
+console.log(`serving from ${port}`)
