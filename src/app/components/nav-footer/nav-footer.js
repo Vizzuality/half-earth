6202176ef55b736ca0fc1b11bce65bbdc093aa3b
Victor Saiz <vs@vsaiz.me>
2017-10-23 14:37:38 +0200

    upload new logo version

--- src/app/components/nav-footer/nav-footer.js
+++ src/app/components/nav-footer/nav-footer.js
@@ -1,16 +1,7 @@
-import { actions as popUpActions } from 'components/pop-up'
-import { connect } from 'react-redux'
-
 import reducers from './nav-footer-reducers'
 import initialState from './initial-state'
 
 import NavFooter from './nav-footer-component'
 
-const mapStateToProps = ({ navFooter }) => ({
-  popUp: navFooter.popUp
-})
-
 export { reducers, initialState }
-export default connect(mapStateToProps, {
-  ...popUpActions
-})(NavFooter)
+export default NavFooter
