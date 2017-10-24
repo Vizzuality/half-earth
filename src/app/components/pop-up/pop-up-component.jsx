474760e358e8dbd04bded1ec0f2a7dd4cefad19d
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-10-24 19:34:34 +0200

    fix: render pop-up inside a React portal to avoid z-index fights

--- src/app/components/pop-up/pop-up-component.jsx
+++ src/app/components/pop-up/pop-up-component.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import cx from 'classnames'
 import CloseButton from './close-button/close-button-component'
 
 import theme from './turquoise-close-button-theme.scss'
 import styles from './pop-up-styles.scss'
 
-const PopUp = ({ className, open, close, children }) => {
-  return (
-    (open && (
-      <article className={cx([className, styles.popUp])}>
+class PopUp extends Component {
+  constructor (props) {
+    super(props)
+    this.root = document.getElementById('pop-up-root')
+    this.el = document.createElement('article')
+  }
+  componentDidMount () {
+    this.root.appendChild(this.el)
+  }
+
+  componentWillUnmount () {
+    this.root.removeChild(this.el)
+  }
+
+  render () {
+    const { className, open, close, children } = this.props
+    const portalContent = (
+      <div className={cx([className, styles.popUp])}>
         <CloseButton theme={theme} close={close} />
         <div className={styles.content}>{children}</div>
-      </article>
-    )) ||
-    null
-  )
+      </div>
+    )
+    return open ? ReactDOM.createPortal(portalContent, this.el) : null
+  }
 }
 export default PopUp
