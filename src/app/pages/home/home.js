e2bd9476b78465cad7435bdc9621d3830f4187bc
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-10-26 16:05:31 +0200

    feat: adds home analytics event

--- src/app/pages/home/home.js
+++ src/app/pages/home/home.js
@@ -1,16 +1,35 @@
 import { Component, createElement } from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+
 import { actions as mapActions } from 'pages/map'
+import { actions as analyticsActions } from 'providers/analytics'
 import HomeComponent from './home-component'
+
+const analytics = {
+  onClick: ['Landing', 'Click Play', 'Click Play']
+}
 
 class HomeContainer extends Component {
   constructor (props) {
     super(props)
     props.resetLayers()
   }
+  onClick = () => {
+    const { history, trackEvent } = this.props
+    trackEvent(...analytics.onClick)
+
+    history.push('/local')
+  }
+
   render () {
-    return createElement(HomeComponent, this.props)
+    return createElement(HomeComponent, {
+      ...this.props,
+      onClick: this.onClick
+    })
   }
 }
 
-export default connect(null, mapActions)(HomeContainer)
+export default withRouter(
+  connect(null, { ...mapActions, ...analyticsActions })(HomeContainer)
+)
