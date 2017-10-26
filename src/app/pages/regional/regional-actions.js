59e329e84184cc1bc57fd209f0ef4e4def87cbed
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-10-26 17:27:09 +0200

    feat: adds biodiversity dropdown events

--- src/app/pages/regional/regional-actions.js
+++ src/app/pages/regional/regional-actions.js
@@ -1,5 +1,11 @@
 import { createAction } from 'redux-actions'
 
-export const selectRegionalSelector = createAction('selectRegionalSelector')
+export const selectRegionalSelector = createAction(
+  'selectRegionalSelector',
+  null,
+  ({ selection, section }) => ({
+    analytics: [section, 'Change species on map', `Change to ${selection}`]
+  })
+)
 export const toggleRegionalLayer = createAction('toggleRegionalLayer')
 export const setRegionalSection = createAction('setRegionalSection')
