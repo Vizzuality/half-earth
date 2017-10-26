9d2c3b85943f6d72f82acc185d0b71db1b41ad22
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-10-26 17:41:37 +0200

    feat: adds global select + toggle events

--- src/app/pages/regional/regional-actions.js
+++ src/app/pages/regional/regional-actions.js
@@ -3,8 +3,8 @@ import { createAction } from 'redux-actions'
 export const selectRegionalSelector = createAction(
   'selectRegionalSelector',
   null,
-  ({ selection, section }) => ({
-    analytics: [section, 'Change species on map', `Change to ${selection}`]
+  ({ selection }) => ({
+    analytics: ['regional', 'Change species on map', `Change to ${selection}`]
   })
 )
 export const toggleRegionalLayer = createAction(
