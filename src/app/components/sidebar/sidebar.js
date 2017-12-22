ecdc369f91b7cd468e6a67e893f3e68097acb498
Victor Saiz <vs@vsaiz.me>
2017-12-22 19:31:31 +0100

    lock scrol when sidepopup is open

--- src/app/components/sidebar/sidebar.js
+++ src/app/components/sidebar/sidebar.js
@@ -4,7 +4,10 @@ import reducers from './sidebar-reducers'
 import initialState from './sidebar-initial-state'
 import SidebarComponent from './sidebar-component'
 
-const mapStateToProps = ({ sidebar }) => sidebar
+const mapStateToProps = ({ sidebar, ...state }) => ({
+  ...sidebar,
+  sidePopupOpen: state.regional.sidePopup.open
+})
 
 export { actions, reducers, initialState }
 export default connect(mapStateToProps, actions)(SidebarComponent)
