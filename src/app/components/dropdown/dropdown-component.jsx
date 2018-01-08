4575e5415771f79f23d9ff3eca0a5cca41b09884
Victor Saiz <vs@vsaiz.me>
2018-01-08 15:25:27 +0100

    make dropdown accept maps

--- src/app/components/dropdown/dropdown-component.jsx
+++ src/app/components/dropdown/dropdown-component.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import cx from 'classnames'
+import isMap from 'lodash/isMap'
 import { themr } from 'react-css-themr'
 
 import styles from './dropdown.scss'
+
+const keys = list => (isMap(list) ? Array.from(list.keys()) : Object.keys(list))
+const get = (value, list) => (isMap(list) ? list.get(value) : list[value])
 
 const Dropdown = ({
   className,
@@ -20,24 +24,22 @@ const Dropdown = ({
       onClick={toggleOpen}
       className={cx(theme.label, { [theme.labelClosed]: closed })}
     >
-      {options[selected]}
+      {get(selected, options)}
     </div>
     <ul className={cx(theme.options, { [theme.optionsClosed]: closed })}>
-      {Object.keys(options)
-        .sort()
-        .map(g => {
-          return (
-            <li
-              key={g}
-              className={cx(theme.option, {
-                [theme.optionSelected]: g === selected
-              })}
-              onClick={() => selectItem(g)}
-            >
-              {options[g]}
-            </li>
-          )
-        })}
+      {keys(options).map((g, ...rest) => {
+        return (
+          <li
+            key={g}
+            className={cx(theme.option, {
+              [theme.optionSelected]: g === selected
+            })}
+            onClick={() => selectItem(g)}
+          >
+            {get(g, options)}
+          </li>
+        )
+      })}
       {!closed && (
         <button className={theme.closeButton} onClick={() => toggleOpen()} />
       )}
