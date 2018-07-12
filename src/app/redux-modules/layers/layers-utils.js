fbb4b9c6824954d67568c341e699990ad816cca7
j8seangel <jseangel@gmail.com>
2018-07-12 20:16:54 +0100

    Helper to get carto url

--- src/app/redux-modules/layers/layers-utils.js
+++ src/app/redux-modules/layers/layers-utils.js
@@ -1,3 +1,31 @@
+export const getCartoUrl = ({ account = 'half-earth', config = {} }) => {
+  const cartoConfig = {
+    version: '1.3.0',
+    layers: [
+      {
+        type: 'mapnik',
+        options: {
+          cartocss_version: '2.3.0',
+          ...config
+        }
+      }
+    ]
+  };
+  return fetch(
+    `https://${account}.carto.com/api/v1/map?config=${encodeURIComponent(
+      JSON.stringify(cartoConfig)
+    )}`
+  )
+    .then(d => d.json())
+    .then(({ layergroupid, cdn_url: { templates: { https: { url } } } }) => ({
+      layergroupid,
+      url
+    }))
+    .then(({ layergroupid, url }) => {
+      return `${url}/${account}/api/v1/map/${layergroupid}/{z}/{x}/{y}.png`;
+    });
+};
+
 export const getMolLayerConfig = name => {
   const data = name.split(':');
   const species = data[0];
