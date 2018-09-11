164a6ca4df74a7d8edb5c6ef5e0f8472b69e671d
Javi Abia <javiabia@eriks-Air.home>
2018-09-11 18:42:10 +0200

    Updated categories list component

--- src/app/pages/root/categories-list/categories-list-component.jsx
+++ src/app/pages/root/categories-list/categories-list-component.jsx
@@ -1,107 +1,106 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import { SwitchInput, Button, Icon, Loading } from 'he-components';
+import { SwitchInput, Button, Icon } from 'he-components';
 
 import infoIcon from 'assets/icons/icon-info.svg';
 import styles from './categories-list-styles';
 
 class CategoriesListComponent extends Component {
   render() {
-    const {
-      loading,
-      categories,
-      handleMetadataClick,
-      handleSwitchChange,
-      handleMultiLayerClick,
-      handleLayerClick
-    } = this.props;
+    const { categories, handleMetadataClick, handleSwitchChange, handleMultiLayerClick, handleLayerClick } = this.props;
 
-    if (loading) return <Loading height={300} />;
     const hasCategories = categories && !!categories.length;
 
     return (
       <div className={styles.container}>
-        {hasCategories &&
-          categories.map(category => (
-            <div className={styles.category} key={category.slug}>
+        {
+          hasCategories && categories.map(category => (
+            <div
+              className={cx(styles.category, { [styles.featuredCategory]: category.featured })}
+              key={category.slug}
+            >
+              {
+                  category.featured &&
+                    <div style={{ backgroundImage: `url(${category.imageUrl})` }} className={styles.featuredImage} />
+                }
               <div className={styles.categorySection}>
                 <h2 className={styles.categoryTitle}>{category.name}</h2>
               </div>
               <div className={styles.categorySection}>
                 <p className={styles.categoryDescription}>{category.description}</p>
-                {category.metadata && (
-                  <Button circle theme={{ button: styles.metadataBtn }} onClick={() => handleMetadataClick(category)}>
-                    <Icon icon={infoIcon} />
-                  </Button>
-                )}
+                {
+                    category.metadata && (
+                    <Button
+                      circle
+                      theme={{ button: styles.metadataBtn }}
+                      onClick={() => handleMetadataClick(category)}
+                    >
+                      <Icon icon={infoIcon} />
+                    </Button>
+                      )
+                  }
               </div>
               {category.datasets.map(dataset => {
-                const { layers, active, slug, name } = dataset;
-                const layersLength = layers && layers.length;
-                if (!layersLength) return null;
-                return (
-                  <div key={slug} className={cx(styles.dataset, { [styles.datasetMultiLayer]: dataset.multilayer })}>
-                    <SwitchInput
-                      key={slug}
-                      id={slug}
-                      checked={active}
-                      onChange={value => handleSwitchChange(category, dataset, value)}
-                      label={name}
-                    />
-                    {active &&
-                      layersLength > 1 && (
-                      <div className={cx({ [styles.multiLayerWrapper]: dataset.multilayer })}>
-                        {layers.map(layer => {
-                          const buttonsMulti = (
-                            <SwitchInput
-                              key={layer.slug}
-                              id={layer.slug}
-                              theme={{ switch: styles.subLayerSwitch }}
-                              label={layer.name}
-                              checked={layer.active}
-                              onChange={() => handleMultiLayerClick(layer)}
-                            />
-                          );
+                  const { layers, active, slug, name } = dataset;
+                  const layersLength = layers && layers.length;
+                  if (!layersLength) return null;
+                  return (
+                    <div key={slug} className={cx(styles.dataset, { [styles.datasetMultiLayer]: dataset.multilayer })}>
+                      <SwitchInput
+                        key={slug}
+                        id={slug}
+                        checked={active}
+                        onChange={value => handleSwitchChange(category, dataset, value)}
+                        label={name}
+                      />
+                      {
+                        active && layersLength > 1 && (
+                        <div className={cx({ [styles.multiLayerWrapper]: dataset.multilayer })}>
+                          {layers.map(layer => {
+                                const buttonsMulti = (
+                                  <SwitchInput
+                                    key={layer.slug}
+                                    id={layer.slug}
+                                    theme={{ switch: styles.subLayerSwitch }}
+                                    label={layer.name}
+                                    checked={layer.active}
+                                    onChange={() => handleMultiLayerClick(layer)}
+                                  />
+                                );
 
-                          const buttonTheme = {
-                            button: layer.active ? styles.buttonActive : styles.button
-                          };
-                          const buttonsUnique = (
-                            <Button
-                              key={layer.slug}
-                              theme={buttonTheme}
-                              onClick={() => handleLayerClick(dataset, layer)}
-                            >
-                              {layer.name}
-                            </Button>
-                          );
-                          return dataset.multilayer ? buttonsMulti : buttonsUnique;
-                        })}
-                      </div>
-                    )}
-                  </div>
-                );
-              })}
+                                const buttonTheme = { button: layer.active ? styles.buttonActive : styles.button };
+                                const buttonsUnique = (
+                                  <Button
+                                    key={layer.slug}
+                                    theme={buttonTheme}
+                                    onClick={() => handleLayerClick(dataset, layer)}
+                                  >
+                                    {layer.name}
+                                  </Button>
+                                );
+                                return dataset.multilayer ? buttonsMulti : buttonsUnique;
+                              })}
+                        </div>
+                          )
+                      }
+                    </div>
+                  );
+                })}
             </div>
-          ))}
+            ))
+        }
       </div>
     );
   }
 }
 
 CategoriesListComponent.propTypes = {
-  loading: PropTypes.bool,
-  categories: PropTypes.array,
+  categories: PropTypes.array.isRequired,
   handleMetadataClick: PropTypes.func.isRequired,
   handleSwitchChange: PropTypes.func.isRequired,
   handleMultiLayerClick: PropTypes.func.isRequired,
   handleLayerClick: PropTypes.func.isRequired
-};
-
-CategoriesListComponent.defaultProps = {
-  loading: false,
-  categories: []
 };
 
 export default CategoriesListComponent;
--- src/app/redux-modules/categories/categories-reducers.js
+++ src/app/redux-modules/categories/categories-reducers.js
@@ -1,32 +1,25 @@
+import { camelCase } from 'lodash';
 import * as actions from './categories-actions';
 
-export const initialState = {
-  loading: false,
-  error: false,
-  data: null
-};
+export const initialState = { loading: false, error: false, data: null };
 
 function setCategoriesLoading(state) {
-  return {
-    ...state,
-    loading: true
-  };
+  return { ...state, loading: true };
 }
 
 function setCategoriesReady(state, { payload }) {
-  return {
-    ...state,
-    loading: false,
-    data: payload
-  };
+  const camelCasedPayload = payload.map(category => {
+    const camelCasedKeys = {};
+    Object.keys(category).forEach(key => {
+      camelCasedKeys[camelCase(key)] = category[key];
+    });
+    return camelCasedKeys;
+  });
+  return { ...state, loading: false, data: camelCasedPayload };
 }
 
 function setCategoriesError(state, { payload }) {
-  return {
-    ...state,
-    loading: false,
-    error: payload
-  };
+  return { ...state, loading: false, error: payload };
 }
 
 export default {
