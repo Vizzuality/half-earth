555c11ee07d3f4c1edf90a35ad780ee718dc30db
Javi Abia <weberjavi@gmail.com>
2018-09-17 12:28:51 +0200

    Add isTouchScreen prop to modal instructions, small style change

--- src/app/components/v2/modal-instructions/modal-instructions.js
+++ src/app/components/v2/modal-instructions/modal-instructions.js
@@ -5,6 +5,9 @@ import ModalInstructionsComponent from './modal-instructions-component';
 
 export const reduxConfig = { actions, reducers, initialState };
 
-const mapStateToProps = ({ modalInstructions }) => ({ isOpen: modalInstructions.isOpen });
+const mapStateToProps = ({ modalInstructions, root }) => ({
+  isOpen: modalInstructions.isOpen,
+  isTouchScreen: root.isTouchScreen
+});
 
 export default connect(mapStateToProps, actions)(ModalInstructionsComponent);
