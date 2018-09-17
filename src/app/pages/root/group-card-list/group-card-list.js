f3b74129882f66084df33ff41e612142c90ed6d4
Javi Abia <weberjavi@gmail.com>
2018-09-17 13:53:44 +0200

    Remove container logic from card list

--- src/app/pages/root/group-card-list/group-card-list.js
+++ src/app/pages/root/group-card-list/group-card-list.js
@@ -1,13 +1,6 @@
-import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { mapStateToProps } from './group-card-list-selectors';
 import GroupCardComponent from './group-card-list-component';
 
-class GroupCardContainer extends Component {
-  render() {
-    return <GroupCardComponent {...this.props} />;
-  }
-}
-
-export default connect(mapStateToProps)(GroupCardContainer);
+export default connect(mapStateToProps)(GroupCardComponent);
