fb2d9a2e6576d9a450e19b9376fe06e981039777
Javi Abia <weberjavi@gmail.com>
2018-09-18 17:14:44 +0200

    Add selector logic and accordion component to group card list

--- src/app/pages/root/group-card-list/group-card-list-component.jsx
+++ src/app/pages/root/group-card-list/group-card-list-component.jsx
@@ -1,68 +1,53 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
-import { groupBy } from 'lodash';
-import { Loading, Icon, Button } from 'he-components';
+import { Loading } from 'he-components';
+import AccordionCard from 'components/v2/accordion-card';
 import chevronIcon from 'assets/icons/chevron.svg';
 import CategoriesList from '../categories-list';
 
 import styles from './group-card-list-styles';
 
 class GroupCardListComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { openGroups: [] };
-  }
-
-  isGroupOpen(group) {
-    return this.state.openGroups.find(g => g === group);
-  }
-
-  toogleGroupActive(group) {
-    if (this.isGroupOpen(group))
-      return this.setState(prevstate => ({ openGroups: prevstate.openGroups.filter(g => g !== group) }));
-    return this.setState(prevstate => ({ openGroups: [ ...prevstate.openGroups, group ] }));
-  }
-
   render() {
-    const { loading, categories } = this.props;
+    const { loading, categoriesGroups, openGroups } = this.props;
     if (loading) return <Loading height={300} />;
-    const hasCategories = categories && !!categories.length;
-    const groups = hasCategories && Object.values(groupBy(categories, 'groupSlug'));
     return (
-      <div className={styles.groupCardListontainer}>
+      <div className={styles.groupCardListContainer}>
         {
-          groups && groups.map(group => (
-            <div
-              key={group[0].groupName}
-              className={cx(styles.cardContainer, {
-                  [styles.customCard]: group[0].groupSlug === 'half-earth-view',
-                  [styles.open]: this.isGroupOpen(group[0].groupSlug)
-                })}
-            >
-              <div className={styles.headerContainer}>
-                <h2 className={styles.groupTitle}>{group[0].groupName}</h2>
-                <Button
-                  onClick={() => this.toogleGroupActive(group[0].groupSlug)}
-                  theme={{ button: styles.toggleButton }}
-                >
-                  <Icon
-                    icon={chevronIcon}
-                    theme={{ icon: this.isGroupOpen(group[0].groupSlug) ? styles.icon : styles.iconCollapse }}
-                  />
-                </Button>
-              </div>
-              <CategoriesList categories={group} />
-            </div>
-            ))
+          categoriesGroups && categoriesGroups.map(
+              group =>
+                group[0].groupSlug !== 'half-earth-view'
+                  ? (
+                    <AccordionCard
+                      key={group[0].groupName}
+                      isOpen={openGroups.includes(group[0].groupSlug)}
+                      title={group[0].groupName}
+                      icon={chevronIcon}
+                    >
+                      <CategoriesList categories={group} />
+                    </AccordionCard>
+)
+                  : (
+                    <div key={group[0].groupName} className={styles.cardContainer}>
+                      <div className={styles.headerContainer}>
+                        <h2 className={styles.groupTitle}>{group[0].groupName}</h2>
+                      </div>
+                      <CategoriesList categories={group} />
+                    </div>
+)
+            )
         }
       </div>
     );
   }
 }
 
-GroupCardListComponent.propTypes = { loading: PropTypes.bool, categories: PropTypes.array };
+GroupCardListComponent.propTypes = {
+  loading: PropTypes.bool,
+  categoriesGroups: PropTypes.array,
+  openGroups: PropTypes.array
+};
 
-GroupCardListComponent.defaultProps = { loading: false, categories: [] };
+GroupCardListComponent.defaultProps = { loading: false, categoriesGroups: [], openGroups: [] };
 
 export default GroupCardListComponent;
--- src/app/pages/root/group-card-list/group-card-list-selectors.js
+++ src/app/pages/root/group-card-list/group-card-list-selectors.js
@@ -1,13 +1,50 @@
+import { groupBy } from 'lodash';
 import { createSelector, createStructuredSelector } from 'reselect';
-import { selectDatasetsLoading } from 'selectors/datasets-selectors';
+import { selectDatasetsLoading, getDatasets } from 'selectors/datasets-selectors';
 import { selectCategoriesLoading, getDatasetsByCategory } from 'selectors/categories-selectors';
+import { selectQueryParams } from 'selectors/location-selectors';
 
 export const getCategoriesLoading = createSelector(
   [ selectDatasetsLoading, selectCategoriesLoading ],
   (datasetsLoading, categoriesLoading) => datasetsLoading && categoriesLoading
 );
 
+const getLayersActive = createSelector([ selectQueryParams ], query => query && query.activeLayers);
+
+export const getCategoriesGroups = createSelector(
+  getDatasetsByCategory,
+  categories => Object.values(groupBy(categories, 'groupSlug'))
+);
+
+export const getGroupCardsOpen = createSelector([ getCategoriesGroups, getLayersActive, getDatasets ], (
+  groups,
+  layersActive,
+  datasets
+) =>
+  {
+    const datasetsInGroups = groups.map(group => {
+      const groupDatasets = [];
+      group.forEach(cat => {
+        groupDatasets.push(cat.datasets.map(dataset => dataset.slug));
+      });
+      return { group: group[0].groupSlug, datasets: groupDatasets.reduce((acc, val) => acc.concat(val), []) };
+    });
+    const openDatasets = datasets &&
+      (datasets.filter(
+        dat =>
+          dat.layers.filter(
+            l => layersActive && layersActive.find(active => active.slug === l.slug) !== undefined
+          ).length >
+            0
+      ) ||
+        []);
+    return datasetsInGroups
+      .filter(g => g.datasets.some(d => openDatasets.find(o => o.slug === d)))
+      .map(group => group.group);
+  });
+
 export const mapStateToProps = createStructuredSelector({
   loading: getCategoriesLoading,
-  categories: getDatasetsByCategory
+  categoriesGroups: getCategoriesGroups,
+  openGroups: getGroupCardsOpen
 });
