68ad45cb406adffaa84f4f2e137cf7ec5aee499a
j8seangel <jseangel@gmail.com>
2018-09-21 12:46:39 +0200

    Show metadata content

--- src/app/components/v2/modal-metadata/modal-metadata-component.jsx
+++ src/app/components/v2/modal-metadata/modal-metadata-component.jsx
@@ -1,17 +1,26 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Modal } from 'he-components';
+import { Modal, Loading } from 'he-components';
 
 class ModalMetadata extends PureComponent {
   render() {
-    const { isOpen, title, setModalMetadataParams, metadata } = this.props;
+    const { isOpen, loading, title, setModalMetadataParams, metadata } = this.props;
     return (
-      <Modal
-        onRequestClose={() => setModalMetadataParams({ isOpen: false })}
-        isOpen={isOpen}
-      >
+      <Modal onRequestClose={() => setModalMetadataParams({ isOpen: false })} isOpen={isOpen}>
         <h2>{title}</h2>
-        {metadata}
+        {loading && <Loading />}
+        {
+          metadata && metadata.length > 0 && (
+          <dl>
+            {metadata.map(meta => (
+              <React.Fragment>
+                <dt><strong>{meta[0]}:</strong></dt>
+                <dd>{meta[1]}</dd>
+              </React.Fragment>
+                ))}
+          </dl>
+            )
+        }
       </Modal>
     );
   }
@@ -19,11 +28,12 @@ class ModalMetadata extends PureComponent {
 
 ModalMetadata.propTypes = {
   title: PropTypes.string,
-  metadata: PropTypes.string,
+  loading: PropTypes.bool,
+  metadata: PropTypes.array,
   isOpen: PropTypes.bool.isRequired,
   setModalMetadataParams: PropTypes.func.isRequired
 };
 
-ModalMetadata.defaultProps = { title: '', data: [], loading: false };
+ModalMetadata.defaultProps = { title: '', loading: false, metadata: null };
 
 export default ModalMetadata;
--- src/app/components/v2/modal-metadata/modal-metadata-selectors.js
+++ src/app/components/v2/modal-metadata/modal-metadata-selectors.js
@@ -2,17 +2,19 @@ import { createSelector, createStructuredSelector } from 'reselect';
 
 const selectOpen = ({ modalMetadata }) => modalMetadata.isOpen;
 const selectTitle = ({ modalMetadata }) => modalMetadata.title;
+const selectLoading = ({ modalMetadata }) => modalMetadata.loading;
 const selectSlug = ({ modalMetadata }) => modalMetadata.slug;
-const selectCategories = ({ categories }) => categories.data;
+const selectData = ({ modalMetadata }) => modalMetadata.data;
 
-export const getMetadata = createSelector([selectSlug, selectCategories], (slug, categories) => {
-  if (!slug || !categories) return;
-  const datasetMeta = categories.find(d => d.slug === slug);
-  return datasetMeta && datasetMeta.metadata;
+export const getMetadata = createSelector([ selectSlug, selectData ], (slug, data) => {
+  if (!slug || !data) return null;
+  const metadata = data[slug];
+  return metadata ? Object.entries(metadata) : null;
 });
 
 export const mapStateToProps = createStructuredSelector({
   isOpen: selectOpen,
+  loading: selectLoading,
   metadata: getMetadata,
   title: selectTitle
 });
