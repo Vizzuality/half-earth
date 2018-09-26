66198271911022accfed9e431181b6108f9e5487
j8seangel <jseangel@gmail.com>
2018-09-26 18:07:49 +0200

    Activate grid detail layers and show percentage wip

--- src/app/pages/root/detail-view/progress-card/progress-card-component.jsx
+++ src/app/pages/root/detail-view/progress-card/progress-card-component.jsx
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ProgressBar, SwitchInput } from 'he-components';
+import { ProgressBar } from 'he-components';
+import DatasetCombo from 'components/v2/dataset-combo';
 
 import styles from './progress-card-styles';
 
 class ProgressBarComponent extends Component {
   render() {
-    const { legend, subtitle, layers } = this.props;
-    const percentage = layers && layers.reduce((acc, next) => acc + next.percentage, 0) || 0;
+    const { legend, subtitle, category } = this.props;
+    const percentage = category ? category.datasets
+        .filter(d => d.active)
+        .reduce((acc, dataset) => acc + dataset.percentage || 0, 0) : 0;
     return (
       <div className={styles.container}>
         <ProgressBar percentage={percentage} label={legend} />
         {subtitle && <p className={styles.subtitle}>{subtitle}</p>}
         {
-          layers && layers.map(layer => (
-            <SwitchInput
-              key={layer.slug}
-              id={layer.slug}
-              checked={layer.active}
-              onChange={() => {
-                  console.info('TODO');
-                }}
-              label={layer.name}
-            />
+          category &&
+            category.datasets.map(dataset => (
+              <DatasetCombo
+                dataset={dataset}
+                category={category}
+                key={dataset.slug}
+                className={styles.datasetComboWrapper}
+              />
             ))
         }
       </div>
@@ -32,11 +33,11 @@ class ProgressBarComponent extends Component {
 
 // eslint-disable-next-line
 ProgressBarComponent.propTypes = {
-  layers: PropTypes.array,
+  category: PropTypes.object,
   legend: PropTypes.string,
   subtitle: PropTypes.string
 };
 
-ProgressBarComponent.defaultProps = { layers: null, subtitle: '', legend: '' };
+ProgressBarComponent.defaultProps = { category: null, subtitle: '', legend: '' };
 
 export default ProgressBarComponent;
