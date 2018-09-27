420b90dd5360fa15afd7bfcb1366f887dc68dd58
j8seangel <jseangel@gmail.com>
2018-09-27 17:16:52 +0200

    Change rarity and richness order

--- src/app/selectors/datasets-selectors.js
+++ src/app/selectors/datasets-selectors.js
@@ -4,28 +4,42 @@ import { selectQueryParams } from 'selectors/location-selectors';
 export const selectDatasets = ({ datasets = {} }) => datasets.data;
 export const selectDatasetsLoading = ({ datasets = {} }) => datasets.loading;
 
-export const getLayersActive = createSelector([ selectQueryParams ], query => query && query.activeLayers);
+export const getLayersActive = createSelector(
+  [ selectQueryParams ],
+  query => query && query.activeLayers
+);
 
-export const getDatasets = createSelector([ selectDatasets, getLayersActive ], (datasets, activeLayers = []) => {
-  if (!datasets) return null;
-  return Object.values(datasets).map(dataset => {
-    let order = 0;
-    const layers = dataset.layers.map(layer => {
-      const layerActive = activeLayers && activeLayers.find(l => l.slug === layer.slug);
-      if (!layerActive) return layer;
+export const getDatasets = createSelector([ selectDatasets, getLayersActive ], (
+  datasets,
+  activeLayers = []
+) =>
+  {
+    if (!datasets) return null;
+    return Object.values(datasets).map(dataset => {
+      let order = 0;
+      const layers = dataset.layers
+        .sort((a, b) => {
+          const isRarityOrRichness = a.name.toUpperCase() === 'RARITY' ||
+            b.name.toUpperCase === 'RICHNESS';
+          if (!isRarityOrRichness) return 0;
+          return b.name.length - a.name.length;
+        })
+        .map(layer => {
+          const layerActive = activeLayers && activeLayers.find(l => l.slug === layer.slug);
+          if (!layerActive) return layer;
 
-      order = activeLayers.findIndex(l => l.slug === layer.slug);
-      return { ...layer, ...layerActive, active: true, zIndex: order + 1 };
+          order = activeLayers.findIndex(l => l.slug === layer.slug);
+          return { ...layer, ...layerActive, active: true, zIndex: order + 1 };
+        });
+      return {
+        ...dataset,
+        active: layers.some(l => l.active),
+        visibility: layers.every(l => l.visibility),
+        order,
+        layers
+      };
     });
-    return {
-      ...dataset,
-      active: layers.some(l => l.active),
-      visibility: layers.every(l => l.visibility),
-      order,
-      layers
-    };
   });
-});
 
 export const getGridDataset = createSelector(getDatasets, datasets => {
   if (!datasets) return null;
