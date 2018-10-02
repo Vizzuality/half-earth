77b566174b9e2eec2cd46b976d91774227410acb
j8seangel <jseangel@gmail.com>
2018-10-02 17:43:32 +0200

    Fix cell id for marine landscapes

--- src/app/services/mol.js
+++ src/app/services/mol.js
@@ -4,9 +4,13 @@ const config = { url: 'https://api.mol.org/1.x/species/info', query: '?scientifi
 
 async function getSpecies(species) {
   const speciesArray = Array.isArray(species) ? species : [ species ];
-  const promises = speciesArray.map(specie => fetch(`${config.url}${config.query}${specie}`).then(d => d.json()));
+  const promises = speciesArray.map(
+    specie => fetch(`${config.url}${config.query}${specie}`).then(d => d.json())
+  );
   const data = await Promise.all(promises);
-  return data && data.map(d => ({ ...d[0], iucn: iucnList[d[0].redlist] }));
+  return data && data
+      .filter(d => !!d.length)
+      .map(d => ({ ...d[0], iucn: iucnList[d[0].redlist] }));
 }
 
 export default { getSpecies };
