042365150d1271cdaa91500174a2e734b9572197
Javi Abia <weberjavi@gmail.com>
2018-10-09 14:58:40 +0200

    mapp tooltip styles

--- src/app/components/v2/map-tooltip/map-tooltip-component.jsx
+++ src/app/components/v2/map-tooltip/map-tooltip-component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Icon } from 'he-components';
 import chevronIcon from 'assets/icons/icon-chevron.svg';
@@ -8,60 +8,75 @@ import styles from './map-tooltip-styles.scss';
 
 const ACTION_TEXT = { story: 'go to story', place: 'go to landscape view' };
 
-const simpleTooltip = ({ title }) => (
-  <div className={styles.simpleContainer}>
-    <span>
-      {'Acadia National Park' || title}
-    </span>
-  </div>
-);
+class MapTooltipComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.height = 450;
+    this.width = 145;
+  }
 
-const storiesAndPlacesTooltip = (
-  { type, title, image, text, handleTooltipAction, handleTooltipClose }
-) => (
-  <div className={styles.container}>
-    <div style={{ backgroundImage: `url(${image}` }} className={styles.image} />
-    <Button onClick={handleTooltipClose} theme={{ button: styles.closeButton }}>
-      <Icon icon={closeIcon} theme={{ icon: styles.closeIcon }} />
-    </Button>
-    <section className={styles.contentSection}>
-      <h2 className={styles.title}>
-        {title}
-      </h2>
-      <p className={styles.text}>
-        {text}
-      </p>
-      <section className={styles.actionSection}>
-        <Button onClick={handleTooltipAction} theme={{ button: styles.linkButton }}>
-          <span className={styles.actionText}>
-            {ACTION_TEXT[type]}
-          </span>
-          <Icon icon={chevronIcon} theme={{ icon: styles.chevronIcon }} />
+  componentDidMount() {
+    this.height = this.tooltip.clientHeight + 25;
+  }
+
+  render() {
+    const { x, y, type, title, image, text, url, handleTooltipClose } = this.props;
+    return (
+      <div
+        className={styles.container}
+        style={{ transform: `translate(${x - this.width}px, ${y - this.height}px)` }}
+        ref={tooltip => {
+          this.tooltip = tooltip;
+        }}
+      >
+        <div style={{ backgroundImage: `url(${image}` }} className={styles.image} />
+        <Button onClick={handleTooltipClose} theme={{ button: styles.closeButton }}>
+          <Icon icon={closeIcon} theme={{ icon: styles.closeIcon }} />
         </Button>
-      </section>
-    </section>
-  </div>
-);
+        <section className={styles.contentSection}>
+          <h2 className={styles.title}>
+            {title}
+          </h2>
+          <p className={styles.text}>
+            {text}
+          </p>
+          <section className={styles.actionSection}>
+            <a
+              href={url}
+              className={styles.linkButton}
+              target="_blank"
+              rel="noopener noreferrer"
+              theme={{ button: styles.linkButton }}
+            >
+              <span className={styles.actionText}>
+                {ACTION_TEXT[type]}
+              </span>
+              <Icon icon={chevronIcon} theme={{ icon: styles.chevronIcon }} />
+            </a>
+          </section>
+        </section>
+      </div>
+    );
+  }
+}
 
-const MapTooltipComponent = ({ simple, ...props }) =>
-  simple ? simpleTooltip(props) : storiesAndPlacesTooltip(props);
-
-storiesAndPlacesTooltip.propTypes = {
+MapTooltipComponent.propTypes = {
   type: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   image: PropTypes.string,
   text: PropTypes.string,
-  handleTooltipAction: PropTypes.func,
+  url: PropTypes.string,
+  x: PropTypes.number,
+  y: PropTypes.number,
   handleTooltipClose: PropTypes.func
 };
 
-simpleTooltip.propTypes = { title: PropTypes.string.isRequired };
-
-storiesAndPlacesTooltip.defaultProps = {
+MapTooltipComponent.defaultProps = {
   image: '',
-  text: 'PropTypes.array',
-  handleTooltipAction: () => {
-  },
+  text: '',
+  url: '#',
+  x: 0,
+  y: 0,
   handleTooltipClose: () => {
   }
 };
