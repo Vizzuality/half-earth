04b0a2afbbe6e9cfea99947a24b77ba478379265
Javi Abia <weberjavi@gmail.com>
2018-10-10 21:57:13 +0200

    Update conservation areas behaviour

--- src/app/pages/root/detail-view/progress-card/progress-card-component.jsx
+++ src/app/pages/root/detail-view/progress-card/progress-card-component.jsx
@@ -5,18 +5,28 @@ import { ProgressBar } from 'he-components';
 import DatasetCombo from 'components/v2/dataset-combo';
 
 import styles from './progress-card-styles';
+import disabledTheme from './progressbar-disabled-styles';
 
 class ProgressBarComponent extends Component {
   render() {
     const { category } = this.props;
+    const { multiSelect } = category;
     const layers = category &&
       category.datasets.reduce((acc, dataset) => [ ...acc, ...dataset.layers ], []);
     const layerActive = layers && layers.find(l => l.active) || {};
+    const disableProgressBar = multiSelect && layers.filter(l => l.active).length > 1;
+    const theme = disableProgressBar ? disabledTheme : {};
     return (
       <div className={cx({ [styles.containerSpaced]: !category.hideProgress })}>
         {
           !category.hideProgress &&
-            <ProgressBar percentage={layerActive.percentage || 0} label={layerActive.name || ''} />
+            (
+              <ProgressBar
+                percentage={layerActive.percentage || 0}
+                label={layerActive.name || ''}
+                theme={theme}
+              />
+            )
         }
         {category.description && <p className={styles.subtitle}>{category.description}</p>}
         {
