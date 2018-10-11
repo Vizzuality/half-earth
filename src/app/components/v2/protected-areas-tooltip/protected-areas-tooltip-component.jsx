7b920f529a92cdfb5208b0f38c301880d0647072
Javi Abia <weberjavi@gmail.com>
2018-10-11 20:10:41 +0200

    Add protected areas tooltip

---
+++ src/app/components/v2/protected-areas-tooltip/protected-areas-tooltip-component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { uniqBy } from 'lodash';
+import styles from './protected-areas-tooltip-styles.scss';
+
+const ProtectedAreasTooltipComponent = ({ x, y, reserves }) => (
+  <div
+    className={styles.container}
+    style={{
+      transform: `translate(calc(${x}px - 50%), calc(${y - 18}px - 100%))`,
+      transformOrigin: 'center bottom'
+    }}
+  >
+    {uniqBy(reserves, 'name').map(r => (
+      <div className={styles.reserve}>
+        <p className={styles.park}>{r.park}</p>
+        <p className={styles.name}>{r.name}</p>
+      </div>
+    ))}
+  </div>
+);
+
+ProtectedAreasTooltipComponent.propTypes = {
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  reserves: PropTypes.array.isRequired
+};
+
+export default ProtectedAreasTooltipComponent;
