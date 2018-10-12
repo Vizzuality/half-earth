479c9013af96ddb55f573a8c7a3bc0af26e150db
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-10-12 19:21:47 +0100

    Link How to navigate to global landscape tutorial

--- src/app/components/v2/modal-instructions/modal-instructions.js
+++ src/app/components/v2/modal-instructions/modal-instructions.js
@@ -1,7 +1,10 @@
 import { connect } from 'react-redux';
-import * as actions from './modal-instructions-actions';
+import { setModalTutorialParams } from 'components/v2/modal-tutorial/modal-tutorial-actions';
+import * as ownActions from './modal-instructions-actions';
 import reducers, { initialState } from './modal-instructions-reducers';
 import ModalInstructionsComponent from './modal-instructions-component';
+
+const actions = { ...ownActions, setModalTutorialParams };
 
 export const reduxConfig = { actions, reducers, initialState };
 
