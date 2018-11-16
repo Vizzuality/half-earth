4634d74be55210b92ae5bf97cc20346b0e406117
Javi Abia <weberjavi@gmail.com>
2018-11-16 14:03:48 +0100

    Add analytics to layerr toggling

--- src/app/components/v2/dataset-combo/dataset-combo-actions.js
+++ src/app/components/v2/dataset-combo/dataset-combo-actions.js
@@ -2,3 +2,13 @@ import { createAction } from 'redux-tools';
 import { APPv2 } from 'router';
 
 export const updateQueryParam = createAction(APPv2);
+
+export const addLayerAnalyticsEvent = createAction('addLayer', null, ({ slug, query }) => {
+  const viewMode = query && query.terrain ? 'Landscape view' : 'Map menu';
+  return { analytics: [ viewMode, 'Add layer', `${slug}` ] };
+});
+
+export const removeLayerAnalyticsEvent = createAction('removeLayer', null, ({ slug, query }) => {
+  const viewMode = query && query.terrain ? 'Landscape view' : 'Map menu';
+  return { analytics: [ viewMode, 'Remove layer', `${slug}` ] };
+});
