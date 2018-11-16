46425454814ecef85ca8be9e7765a3a150b6a9be
Javi Abia <weberjavi@gmail.com>
2018-11-16 13:53:26 +0100

    Add analytics to share modal

--- src/app/components/v2/modal-share/modal-share-actions.js
+++ src/app/components/v2/modal-share/modal-share-actions.js
@@ -1,3 +1,7 @@
 import { createAction } from 'redux-tools';
 
 export const setModalShareParams = createAction('setModalShareParams');
+
+export const urlShareAnalyticsEvent = createAction('shareUrl', null, ({ url, shareType }) => ({
+  analytics: [ 'Share', shareType, `${url}` ]
+}));
