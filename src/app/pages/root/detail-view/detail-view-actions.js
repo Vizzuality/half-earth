db1ce9022d382117eebf60ddc6cb5aa098b7e876
Javi Abia <weberjavi@gmail.com>
2018-11-16 13:52:06 +0100

    Add analytics to detail view

--- src/app/pages/root/detail-view/detail-view-actions.js
+++ src/app/pages/root/detail-view/detail-view-actions.js
@@ -2,3 +2,7 @@ import { createAction } from 'redux-tools';
 import { APPv2 } from 'router';
 
 export const updateQueryParam = createAction(APPv2);
+
+export const taxasChangeAnalyticsEvent = createAction('selectTaxa', null, taxaName => ({
+  analytics: [ 'Landscape view', 'Filter species', `${taxaName}` ]
+}));
