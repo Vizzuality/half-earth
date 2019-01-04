3136d4227890cf83d4cb9d3be33608ada3536167
Javi Abia <weberjavi@gmail.com>
2019-01-04 12:05:00 +0100

    Add legend actions

--- src/app/pages/root/legend/legend-actions.js
+++ src/app/pages/root/legend/legend-actions.js
@@ -2,3 +2,8 @@ import { createAction } from 'redux-tools';
 import { APPv2 } from 'router';
 
 export const updateQueryParam = createAction(APPv2);
+
+export const legendInteractionAnalyticsEvent = createAction('legendInteraction', null, (
+  actionName,
+  layerName
+) => ({ analytics: [ 'Legend', `${actionName}`, `${layerName}` ] }));
